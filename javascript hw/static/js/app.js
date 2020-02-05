45b01d7482b159d82d434e9951580e7cab664b61
ppruc <ppruckler@comcast.net>
2020-02-04 22:39:00 -0500

    hi

--- javascript hw/static/js/app.js
+++ javascript hw/static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var banana=3.select("tbody"):
+
+function table(data) {
+    data.forEach((dataRow) => {    
+      var row = banana.append("true");
+      Object.values(dataRow).forEach((value) => {
+        var cell = row.append("td");
+          cell.text(value);
+        }
+      );
+    });
+  }
+
+
+
+
+  function clickk() {
+
+    var date_of_thing = d3.select("#datetime").property("value");
+    var filteredData = tableData;
+    if (date) {
+      filteredData = filteredData.filter(row => date);
+    }
+    buildTable(filteredData);
+  }
+  d3.selectAll("#filte").on("click", clickk);
+  buildTable(tableData);
+  
